12a5e9a9337d190a32def596c70db90ef244accb
ertan <ertan@profitwolf.eu>
2017-12-02 21:25:58 +0000

    working with django 1.10

--- src/timelog/management/commands/analyze_timelog.py
+++ src/timelog/management/commands/analyze_timelog.py
@@ -1,24 +1,16 @@
-from optparse import make_option
-
-from django.core.management.base import BaseCommand
 from django.conf import settings
-
+from django.core.management.base import BaseCommand
 from timelog.lib import generate_table_from, analyze_log_file, PATTERN
 
 
 class Command(BaseCommand):
-
-    option_list = BaseCommand.option_list + (
-        make_option('--file',
-            dest='file',
-            default=settings.TIMELOG_LOG,
-            help='Specify file to use'),
-        make_option('--noreverse',
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--noreverse',
             dest='reverse',
             action='store_false',
             default=True,
-            help='Show paths instead of views'),
-        )
+            help='Show paths instead of views')
 
     def handle(self, *args, **options):
         # --date-from YY-MM-DD
@@ -28,7 +20,7 @@ class Command(BaseCommand):
         #   specify a date filter end
         #   default to now
 
-        LOGFILE = options.get('file')
+        LOGFILE = settings.TIMELOG_LOG
 
         try:
             data = analyze_log_file(LOGFILE, PATTERN, reverse_paths=options.get('reverse'))
--- src/timelog/middleware.py
+++ src/timelog/middleware.py
@@ -14,7 +14,6 @@ class TimeLogMiddleware(object):
 
     def __call__(self, request):
         start = timeit.default_timer()
-
         response = self.get_response(request)
 
         sqltime = 0.0
@@ -22,15 +21,14 @@ class TimeLogMiddleware(object):
         for q in connection.queries:
             sqltime += float(getattr(q, 'time', 0.0))
 
-        if hasattr(request, '_start'):
-            d = {
-                'method': request.method,
-                'time': timeit.default_timer() - start,
-                'code': response.status_code,
-                'url': smart_str(request.path_info),
-                'sql': len(connection.queries),
-                'sqltime': sqltime,
-            }
-            msg = '%(method)s "%(url)s" (%(code)s) %(time).2f (%(sql)dq, %(sqltime).4f)' % d
-            logger.info(msg)
+        d = {
+            'method': request.method,
+            'time': timeit.default_timer() - start,
+            'code': response.status_code,
+            'url': smart_str(request.path_info),
+            'sql': len(connection.queries),
+            'sqltime': sqltime,
+        }
+        msg = '%(method)s "%(url)s" (%(code)s) %(time).2f (%(sql)dq, %(sqltime).4f)' % d
+        logger.info(msg)
         return response
